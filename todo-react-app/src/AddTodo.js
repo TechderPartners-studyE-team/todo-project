42ecd4a7b7858725aae516a5ce0868b77499120d
EASYhz <jiyon3@gachon.ac.kr>
2022-12-21 01:34:03 +0900

    Frontend: v2

--- todo-react-app/src/AddTodo.js
+++ todo-react-app/src/AddTodo.js
@@ -4,6 +4,13 @@ import { Button, Grid, TextField } from "@mui/material";
 const AddTodo = (props) => {
     // 사용자의 입력을 저장할 오브젝트
     const [item, setItem] = useState({ title: ""});
+    const addItem = props.addItem;
+
+    // onButtonClick 함수 작성
+    const onButtonClick = () => {
+        addItem(item);
+        setItem({title: ""});
+    }
 
     // onInputChange 함수 작성
     const onInputChange = (e) => {
@@ -19,7 +26,8 @@ const AddTodo = (props) => {
                     onChange={onInputChange} value={item.title} />
             </Grid>
             <Grid xs={1} md={1} item>
-                <Button fullWidth style={{height: "100%"}} color="secondary" varient="outlined">
+                <Button fullWidth style={{height: "100%"}} color="secondary" varient="outlined"
+                    onClick={onButtonClick}>
                     +
                 </Button>
             </Grid>
--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -17,6 +17,15 @@ function App() {
       done: true,
     },]
   );
+
+  const addItem = (item) => {
+    item.id = "ID-" + items.length; // key를 위한 id
+    item.done = false; // done 초기화
+    // 업데이트는 반드시 setItems로 하고 새 배열을 만들어야 함.
+    setItems([...items, item]);
+
+    console.log("items : ", items);
+  }
   let todoItems = items.length > 0 && (
     <Paper style={{ margin: 16}}>
       <List>
@@ -29,7 +38,7 @@ function App() {
 
   return (
     <div className="App">
-      <AddTodo />
+      <AddTodo addItem={addItem}/>
       <div className='TodoList'>{ todoItems }</div>
     </div>
   );
