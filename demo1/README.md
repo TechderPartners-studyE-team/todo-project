dae45702155fa7357ea7c4e8bf125d0a5898d822
EASYhz <jiyon3@gachon.ac.kr>
2022-12-25 00:25:35 +0900

    Chapter 3: Backend & Frontend

--- demo1/README.md
+++ demo1/README.md
@@ -1,3 +1,8 @@
 # todo-project
 이 프로젝트는 앱101 책을 바탕으로 만든 Todo애플리케이션입니다.
-브랜치 생성 후 현재 진행상황까지 업로드하면 됩니다. 
+
+## BACKEND
+1. CREATE
+2. READ
+3. UPDATE
+4. DELETE
--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,30 +1,45 @@
 import './App.css';
 import Todo from './Todo';
 import AddTodo from './AddTodo'
-import { useState } from 'react';
-import { List, Paper } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Container, List, Paper } from '@mui/material';
+import { call } from './ApiService';
 
 function App() {
   const [items, setItems] = useState([]);
 
-  const addItem = (item) => {
-    item.id = "ID-" + items.length; // key를 위한 id
-    item.done = false; // done 초기화
-    // 업데이트는 반드시 setItems로 하고 새 배열을 만들어야 함.
-    setItems([...items, item]);
+  useEffect(() => {
+    call("/todo", "GET", null)
+      .then((response) => setItems(response.data));
+  }, []);
 
-    console.log("items : ", items);
+  
+
+  const addItem = (item) => {
+    // item.id = "ID-" + items.length; // key를 위한 id
+    // item.done = false; // done 초기화
+    // // 업데이트는 반드시 setItems로 하고 새 배열을 만들어야 함.
+    // setItems([...items, item]);
+    call("/todo", "POST", item)
+      .then((response) => setItems(response.data));
+    
+      console.log("items : ", items);
   }
 
   const deleteItem = (item) => {
-    // 삭제할 아이템을 찾음.
-    const newItems = items.filter(e => e.id !== item.id);
-    // 삭제할 아이템을 제외한 아이템을 다시 배열에 저장
-    setItems([...newItems]);
+    // // 삭제할 아이템을 찾음.
+    // const newItems = items.filter(e => e.id !== item.id);
+    // // 삭제할 아이템을 제외한 아이템을 다시 배열에 저장
+    // setItems([...newItems]);
+
+    call("/todo", "DELETE", item)
+      .then((response) => setItems(response.data));
   }
 
-  const editItem = () => {
-    setItems([...items]);
+  const editItem = (item) => {
+    // setItems([...items]);
+    call("/todo", "PUT", item)
+      .then((response) => setItems(response.data));
   }
 
   let todoItems = items.length > 0 && (
@@ -43,8 +58,10 @@ function App() {
 
   return (
     <div className="App">
-      <AddTodo addItem={addItem}/>
-      <div className='TodoList'>{ todoItems }</div>
+      <Container maxwidth="md">
+        <AddTodo addItem={addItem}/>
+        <div className='TodoList'>{ todoItems }</div>
+      </Container>
     </div>
   );
 }
